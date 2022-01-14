600aa8505cb2bc755707acd6507494731a70846b
AlinaKorn <alina.vertola@mail.ru>
2022-01-14 12:16:12 +0300

    Fix images path

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,7 +27,7 @@
             <nav class="md:flex md:justify-between md:items-center">
                 <div>
                     <a href="/">
-                        <img src="/images/logo.svg" alt="Laracasts Logo" width="165" height="16">
+                        <img src="{{ asset('storage/images/logo.svg') }}" alt="Laracasts Logo" width="165" height="16">
                     </a>
                 </div>
 
@@ -63,7 +63,7 @@
             </main>
 
             <footer id="newsletter" class="bg-gray-100 border border-black border-opacity-5 rounded-xl text-center py-16 px-10 mt-16">
-                <img src="/images/lary-newsletter-icon.svg" alt="" class="mx-auto -mb-6" style="width: 145px;">
+                <img src="{{ asset('storage/images/lary-newsletter-icon.svg') }}" alt="" class="mx-auto -mb-6" style="width: 145px;">
                 <h5 class="text-3xl">Stay in touch with the latest posts</h5>
                 <p class="text-sm mt-3">Promise to keep the inbox clean. No bugs.</p>
 
@@ -74,7 +74,7 @@
                             @csrf
                             <div class="lg:py-3 lg:px-5 flex items-center">
                                 <label for="email" class="hidden lg:inline-block">
-                                    <img src="/images/mailbox-icon.svg" alt="mailbox letter">
+                                    <img src="{{ asset('storage/images/mailbox-icon.svg') }}" alt="mailbox letter">
                                 </label>
                                 <div>
                                     <input id="email" name="email" type="email" placeholder="Your email address"
