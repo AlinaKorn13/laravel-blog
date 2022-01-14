fd2b8d1fb85de6c9663d980623b42abcc076d297
AlinaKorn <alina.vertola@mail.ru>
2022-01-14 14:36:14 +0300

    Fix button Read more

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -31,9 +31,9 @@
             <footer class="flex justify-between items-center mt-8">
                 <x-author-avatar :post="$post" />
 
-                <div>
+                <div class="flex">
                     <a href="/posts/{{ $post->slug }}"
-                       class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8"
+                       class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8 w-max"
                     >Read More</a>
                 </div>
             </footer>
